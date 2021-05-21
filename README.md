b6fc6a4ce85c0e583428fcd7270084a66f0b14e8
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2021-05-21 10:52:33 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,11 +1,67 @@
 # Elastic Weight Consolidation 
+Standard neural networks can't learn continually without forgetting previous tasks ('Catastrophic Forgetting').  
+Here, I implemented a method called Elastic Weight Consolidation (EWC), originally proposed by Kirkpatrick et al (2017), that mitigates this problem.
 
-This is a Python implementation of the EWC algorithm as proposed by Kirkpatrick et al, 2017      
-
-## Evaluation
-Below a performance comparison between networks trained either with pure SGD or additionally with EWC, evaluated on two permuted mnist tasks. 
+## Example
+Below a performance comparison between networks trained either with pure SGD or additionally with EWC, evaluated on two permuted MNIST tasks. 
 
 <img src="performance_sgd.png" alt="Network without EWC" width="400"/> <img src="performance_ewc.png" alt="Network with EWC" width="400"/>
 <!-- ![Network without EWC](performance_sgd.png =100x)    
 ![Network with EWC](performance_ewc.png =100x) -->
+
+
+
+## How to run the code 
+The code is modularised. Call run_ewc.py to train the network first on standard mnist and then on permuted mnist. A plot with learning curves for held-out data will be generated automatically. 
+```bash
+python run_ewc.py
+```
+
+You can specify the following settings in run_ewc.py:
+
+```python
+params['n_inputs'] = 784 # number of input nodes
+params['n_classes'] = 10 # number of classes (digits 0 to 9)
+params['n_hidden'] = 100 # number of hidden units
+params['weight_init'] = 1e-2 # scale of the weight variance
+
+params['n_iters'] = int(1e4) # number of training epochs
+
+params['lrate'] = 1e-1 # learning rate
+params['do_ewc'] = True # perform ewc (set to false to run vanilla sgd)
+params['ewc_lambda'] = 15 # lambda hyperparameter for ewc loss
+params['fim_samples'] = 500 # number of samples used to compute fisher information matrix
+params['mbatch_size'] = 250 # minibatch size 
+
+params['disp_n_steps'] = 100 # display progress every n steps
+params['verbose'] = True # print some training data to stdout
+
+```
+
+## How to embed the code in your project 
+The code is modularised as follows:
+
+```python
+from ewc_lib.model import Nnet # contains neural network
+from ewc_lib.trainer import train_nnet # contains training routines
+from ewc_lib.visualise import disp_results # simple function to plot learning curves
+```
+
+
+
+## Requirements
+This implementation is based on tensorflow 1.x. Feel free to contribute a port to tf2 if you fancy.
+It's guaranteed to work with the following package versions:
+```
+tensorflow==1.15.0
+matplotlib==3.1.3
+numpy==1.18.1
+scikit_learn==0.24.2
+```
+
+
+## Acknowledgements
+Thanks to Ari Seff's implementation of EWC which clarified a few issues I faced when setting up the computational graph
+
+
 
--- ewc_lib/trainer.py
+++ ewc_lib/trainer.py
@@ -6,6 +6,16 @@ from ewc_lib.data import load_mnist_data, permute_mnist
 from ewc_lib.model import Nnet
 
 
+def train_step(nnet,mbatch):    
+    nnet.train_step.run(feed_dict={nnet.x_features:mbatch[0],nnet.y_true:mbatch[1]})
+
+def eval_step(nnet,dataset1,dataset2,ep,results,verbose):
+    results['acc1'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset1.test.images,nnet.y_true:dataset1.test.labels}))
+    results['acc2'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset2.test.images,nnet.y_true:dataset2.test.labels}))
+    if verbose:
+        print('episode {:d} on 1st task: accuracy 1st task {:2f}, accuracy 2nd task {:2f}'.format(ep,results['acc1'][-1],results['acc2'][-1]))
+
+
 def train_nnet(params):
     '''
     trains neural network 
@@ -19,6 +29,7 @@ def train_nnet(params):
     dataset1 = load_mnist_data()
     # now create permuted mnist 
     dataset2 = permute_mnist(dataset1)
+
     config = tf.ConfigProto()
     config.gpu_options.allow_growth=True
 
@@ -32,37 +43,21 @@ def train_nnet(params):
                             ewc_lamb=params['ewc_lambda'],
                             n_samples=params['fim_samples'])
         sess.run(tf.global_variables_initializer())
-        # train on mnist 
+
+        # train on first task 
         for ep in range(params['n_iters']):
-            mbatch = dataset1.train.next_batch(params['mbatch_size'])
-            nnet.train_step.run(feed_dict={nnet.x_features:mbatch[0],nnet.y_true:mbatch[1]})
+            train_step(nnet,dataset1.train.next_batch(params['mbatch_size']))
             if ep%params['disp_n_steps']==0:
-                results['acc1'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset1.test.images,nnet.y_true:dataset1.test.labels}))
-                results['acc2'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset2.test.images,nnet.y_true:dataset2.test.labels}))
-                if params['verbose']:
-                    print('episode {:d} on 1st task: accuracy 1st task {:2f}, accuracy 2nd task {:2f}'.format(ep,results['acc1'][-1],results['acc2'][-1]))
+                eval_step(nnet,dataset1,dataset2,ep,results,params['verbose'])
         
+        # train on second task
         if params['do_ewc']:
             # ... run network with ewc:            
             nnet.switch_to_ewc(dataset1.train.images)
-            for ep in range(params['n_iters']):
-                mbatch = dataset2.train.next_batch(params['mbatch_size'])
-                nnet.train_step.run(feed_dict={nnet.x_features:mbatch[0],nnet.y_true:mbatch[1]})
-                if ep%params['disp_n_steps']==0:
-                    results['acc1'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset1.test.images,nnet.y_true:dataset1.test.labels}))
-                    results['acc2'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset2.test.images,nnet.y_true:dataset2.test.labels}))
-                    if params['verbose']: 
-                        print('episode {:d} on 2nd task: accuracy 1st task {:2f}, accuracy 2nd task {:2f}'.format(ep,results['acc1'][-1],results['acc2'][-1]))
-            
-        else:
-            # ... run vanilla sgd: 
-            for ep in range(params['n_iters']):
-                mbatch = dataset2.train.next_batch(params['mbatch_size'])
-                nnet.train_step.run(feed_dict={nnet.x_features:mbatch[0],nnet.y_true:mbatch[1]})
-                if ep%params['disp_n_steps']==0:
-                    results['acc1'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset1.test.images,nnet.y_true:dataset1.test.labels}))
-                    results['acc2'].append(nnet.accuracy.eval(feed_dict={nnet.x_features:dataset2.test.images,nnet.y_true:dataset2.test.labels}))
-                    if params['verbose']:
-                        print('episode {:d}on 2nd task: accuracy 1st task {:2f}, accuracy 2nd task {:2f}'.format(ep,results['acc1'][-1],results['acc2'][-1]))
+
+        for ep in range(params['n_iters']):
+            train_step(nnet,dataset2.train.next_batch(params['mbatch_size']))
+            if ep%params['disp_n_steps']==0:
+                eval_step(nnet,dataset1,dataset2,ep,results,params['verbose'])
 
     return results
